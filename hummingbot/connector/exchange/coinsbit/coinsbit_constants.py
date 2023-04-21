414eb7b511d6cf4cce59c1d135de3c01c51e4efa
Sina Amini <general.sina.amini.20@gmai.com>
2023-04-21 12:04:42 +0330

    coinsbit constants

---
+++ hummingbot/connector/exchange/coinsbit/coinsbit_constants.py
@@ -0,0 +1,14 @@
+# General Information
+EXCHANGE_NAME = 'coinsbit'
+DEFAULT_DOMAIN = 'io'
+
+# Base URL
+REST_URL = 'https://coinsbit.{}/api/'
+WSS_URL = 'wss://coinsbit.{}/trade_ws'
+
+PUBLIC_API_VERSION = 'v1'
+PRIVATE_API_vERSION = 'v1'
+
+# Private API endpoints
+ACCOUNT_BALANCES_PATH = '/account/balances'
+ACCOUNT_BALANCE_PATH = '/account/balance'
