0f6648b8f69878decb871fbdc5c279e5693c0d94
Sina Amini <general.sina.amini.20@gmai.com>
2023-04-25 20:57:02 +0330

    get_current_server_time, build_api_factory_without_time_synchronizer_pre_processor, create_throttler, build_api_factory functions added

--- hummingbot/connector/exchange/coinsbit/coinsbit_web_utils.py
+++ hummingbot/connector/exchange/coinsbit/coinsbit_web_utils.py
@@ -1,11 +1,19 @@
+from typing import Optional
+
 import hummingbot.connector.exchange.coinsbit.coinsbit_constants as CONSTANTS
+from hummingbot.connector.time_synchronizer import TimeSynchronizer
+from hummingbot.connector.utils import TimeSynchronizerRESTPreProcessor
+from hummingbot.core.api_throttler.async_throttler import AsyncThrottler
+from hummingbot.core.web_assistant.auth import AuthBase
+from hummingbot.core.web_assistant.connections.data_types import WSJSONRequest
+from hummingbot.core.web_assistant.web_assistants_factory import WebAssistantsFactory, WSAssistant
 
 
 def public_rest_url(path_url: str, domain: str = CONSTANTS.DEFAULT_DOMAIN) -> str:
     """
     Creates a full URL for provided public REST endpoint
     :param path_url: a public REST endpoint
-    :param domain: the Binance domain to connect to ("com" or "us"). The default value is "com"
+    :param domain: the Coinsbit domain to connect to ("io"). The default value is "io"
     :return: the full URL to the endpoint
     """
     return CONSTANTS.REST_URL.format(domain) + CONSTANTS.PUBLIC_API_VERSION + path_url
@@ -15,7 +23,55 @@ def private_rest_url(path_url: str, domain: str = CONSTANTS.DEFAULT_DOMAIN) -> s
     """
     Creates a full URL for provided private REST endpoint
     :param path_url: a private REST endpoint
-    :param domain: the Binance domain to connect to ("com" or "us"). The default value is "com"
+    :param domain: the Coinsbit domain to connect to ("io"). The default value is "io"
     :return: the full URL to the endpoint
     """
     return CONSTANTS.REST_URL.format(domain) + CONSTANTS.PRIVATE_API_VERSION + path_url
+
+
+def build_api_factory(
+        throttler: Optional[AsyncThrottler] = None,
+        time_synchronizer: Optional[TimeSynchronizer] = None,
+        domain: str = CONSTANTS.DEFAULT_DOMAIN,
+        time_provider: Optional[callable] = None,
+        auth: Optional[AuthBase] = None) -> WebAssistantsFactory:
+    throttler = throttler or create_throttler()
+    time_synchronizer = time_synchronizer or TimeSynchronizer()
+    time_provider = time_provider or (lambda: get_current_server_time(
+        throttler=throttler,
+        domain=domain,
+    ))
+    api_factory = WebAssistantsFactory(
+        throttler=throttler,
+        auth=auth,
+        rest_pre_processors=[
+            TimeSynchronizerRESTPreProcessor(synchronizer=time_synchronizer, time_provider=time_provider)
+        ])
+    return api_factory
+
+
+def build_api_factory_without_time_synchronizer_pre_processor(throttler: AsyncThrottler) -> WebAssistantsFactory:
+    api_factory = WebAssistantsFactory(throttler=throttler)
+    return api_factory
+
+
+def create_throttler() -> AsyncThrottler:
+    return AsyncThrottler(CONSTANTS.RATE_LIMIT)
+
+
+async def get_current_server_time(
+        throttler: Optional[AsyncThrottler] = None,
+        domain: str = CONSTANTS.DEFAULT_DOMAIN,
+) -> float:
+    throttler = throttler or create_throttler()
+    api_factory = build_api_factory_without_time_synchronizer_pre_processor()
+    ws_assistant: WSAssistant = await api_factory.get_ws_assistant()
+    await ws_assistant.connect(
+        ws_url=CONSTANTS.WSS_URL.format(domain),
+        ping_timeout=CONSTANTS.WS_HEARTBEAT_TIME_INTERVAL)
+    payload = {"method": "server.time", "params": [], "id": 0}
+    req = WSJSONRequest(payload=payload)
+    await ws_assistant.send(req)
+    server_time = await ws_assistant.receive()['result']
+    await ws_assistant.disconnect()
+    return server_time
