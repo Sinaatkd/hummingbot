49bf8d13fdf96c51ef19c8e2e120727a680907aa
Sina Amini <general.sina.amini.20@gmai.com>
2023-04-25 16:39:33 +0330

    build private rest url and public rest url function added in to coinsbit_web_utils.py

---
+++ hummingbot/connector/exchange/coinsbit/coinsbit_web_utils.py
@@ -0,0 +1,21 @@
+import hummingbot.connector.exchange.coinsbit.coinsbit_constants as CONSTANTS
+
+
+def public_rest_url(path_url: str, domain: str = CONSTANTS.DEFAULT_DOMAIN) -> str:
+    """
+    Creates a full URL for provided public REST endpoint
+    :param path_url: a public REST endpoint
+    :param domain: the Binance domain to connect to ("com" or "us"). The default value is "com"
+    :return: the full URL to the endpoint
+    """
+    return CONSTANTS.REST_URL.format(domain) + CONSTANTS.PUBLIC_API_VERSION + path_url
+
+
+def private_rest_url(path_url: str, domain: str = CONSTANTS.DEFAULT_DOMAIN) -> str:
+    """
+    Creates a full URL for provided private REST endpoint
+    :param path_url: a private REST endpoint
+    :param domain: the Binance domain to connect to ("com" or "us"). The default value is "com"
+    :return: the full URL to the endpoint
+    """
+    return CONSTANTS.REST_URL.format(domain) + CONSTANTS.PRIVATE_API_VERSION + path_url
