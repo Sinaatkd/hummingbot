53639feb84679a3fae87aad4123d9978c8829290
Sina Amini <general.sina.amini.20@gmai.com>
2023-04-28 17:52:41 +0330

    diff and trade event type and  snapshot path url added in to coinsbit_constants.py

--- hummingbot/connector/exchange/coinsbit/coinsbit_constants.py
+++ hummingbot/connector/exchange/coinsbit/coinsbit_constants.py
@@ -9,11 +9,18 @@ WSS_URL = 'wss://coinsbit.{}/trade_ws'
 PUBLIC_API_VERSION = 'v1'
 PRIVATE_API_vERSION = 'v1'
 
+# Public API endpoints
+SNAPSHOT_PATH_URL = '/public/depth/result'
+
 # Private API endpoints
 ACCOUNT_BALANCES_PATH = '/account/balances'
 ACCOUNT_BALANCE_PATH = '/account/balance'
 
 WS_HEARTBEAT_TIME_INTERVAL = 30
+
+# Websocket event types
+DIFF_EVENT_TYPE = "depth.update"
+TRADE_EVENT_TYPE = "deals.update"
 
 # Coinsbit params
 SIDE_BUY = "buy"
